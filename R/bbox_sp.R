4a4f6ade9fcc6d6ebe8c49631e850365c88384c6
PC 7 <PC 7@131.220.67.49>
2019-02-06 09:42:41 +0100

    Adaption of bbox_sp to Extent

--- R/bbox_sp.R
+++ R/bbox_sp.R
@@ -3,18 +3,21 @@
 # Author: Miguel Alvarez
 ################################################################################
 
-bbox_sp <- function(raster, geom="point") {
+bbox_sp <- function(raster, geom="point", proj4) {
 	geom <- pmatch(geom, c("point", "polygon"))
 	if(!geom %in% c(1,2))
 		stop("Invalid value for argument 'geom'.")
-	if(class(raster) != "Extent") ext <- extent(raster) else ext <- raster
+	if(class(raster) != "Extent") {
+		ext <- extent(raster)
+		proj4 <- proj4string(raster)
+	} else ext <- raster
 	# Extension as points
 	if(geom == 1) {
 		ext <- data.frame(fid=1:4, longitude=c(ext@xmin, ext@xmin, ext@xmax,
 						ext@xmax), latitude=c(ext@ymin, ext@ymax, ext@ymax,
 						ext@ymin))
 		coordinates(ext) <- ~ longitude + latitude
-		proj4string(ext) <- CRS(proj4string(raster))
+		proj4string(ext) <- CRS(proj4)
 	}
 	# Extension as polygon
 	if(geom == 2) {

3cbdf0a688333f4cc18bf7fe1a4d376ed9dee34e
Miguel Alvarez <kamapu78@gmail.com>
2019-02-19 23:53:30 +0100

    Functions converted to methods

--- R/bbox_sp.R
+++ R/bbox_sp.R
@@ -3,30 +3,59 @@
 # Author: Miguel Alvarez
 ################################################################################
 
-bbox_sp <- function(raster, geom="point", proj4) {
-	geom <- pmatch(geom, c("point", "polygon"))
-	if(!geom %in% c(1,2))
-		stop("Invalid value for argument 'geom'.")
-	if(class(raster) != "Extent") {
-		ext <- extent(raster)
-		proj4 <- proj4string(raster)
-	} else ext <- raster
-	# Extension as points
-	if(geom == 1) {
-		ext <- data.frame(fid=1:4, longitude=c(ext@xmin, ext@xmin, ext@xmax,
-						ext@xmax), latitude=c(ext@ymin, ext@ymax, ext@ymax,
-						ext@ymin))
-		coordinates(ext) <- ~ longitude + latitude
-		proj4string(ext) <- CRS(proj4)
-	}
-	# Extension as polygon
-	if(geom == 2) {
-		ext <- Polygons(list(Polygon(cbind(c(ext@xmin, ext@xmin, ext@xmax,
-												ext@xmax), c(ext@ymin, ext@ymax,
-												ext@ymax, ext@ymin)))),
-				ID="bbox")
-		ext <- SpatialPolygons(list(ext), proj4string=CRS(proj4))
-		ext <- SpatialPolygonsDataFrame(ext, data.frame(fid=1), match.ID=FALSE)
-	}
-	return(ext)
-}
+# Generic function
+setGeneric("bbox_sp",
+		function(raster, ...)
+			standardGeneric("bbox_sp")
+)
+
+# Method for class Extent
+setMethod("bbox_sp", signature(raster="Extent"),
+		function(raster, proj4="", geom="point", ...) {
+			geom <- pmatch(geom, c("point", "polygon"))
+			if(!geom %in% c(1,2))
+				stop("Invalid value for argument 'geom'.")
+			# Extension as points
+			if(geom == 1) {
+				raster <- data.frame(
+						fid=1:4,
+						longitude=c(raster@xmin, raster@xmin, raster@xmax,
+								raster@xmax),
+						latitude=c(raster@ymin, raster@ymax, raster@ymax,
+								raster@ymin))
+				coordinates(raster) <- ~ longitude + latitude
+				proj4string(raster) <- CRS(proj4)
+			}
+			# rasterension as polygon
+			if(geom == 2) {
+				raster <- Polygons(list(Polygon(cbind(c(raster@xmin, raster@xmin,
+														raster@xmax, raster@xmax),
+												c(raster@ymin, raster@ymax,
+														raster@ymax, raster@ymin)))),
+						ID="bbox")
+				raster <- SpatialPolygons(list(raster), proj4string=CRS(proj4))
+				raster <- SpatialPolygonsDataFrame(raster, data.frame(fid=1),
+						match.ID=FALSE)
+			}
+			return(raster)
+		}
+)
+
+# Method for class RasterLayer etc.
+setMethod("bbox_sp", signature(raster="RasterLayer"),
+		function(raster, geom="point", ...) {
+			return(bbox_sp(extent(raster), proj4string(raster), geom, ...))
+		}
+)
+
+setMethod("bbox_sp", signature(raster="RasterStack"),
+		function(raster, geom="point", ...) {
+			return(bbox_sp(extent(raster), proj4string(raster), geom, ...))
+		}
+)
+
+setMethod("bbox_sp", signature(raster="RasterBrick"),
+		function(raster, geom="point", ...) {
+			return(bbox_sp(extent(raster), proj4string(raster), geom, ...))
+		}
+)
--- R/pixel_dim.R
+++ R/pixel_dim.R
@@ -3,17 +3,26 @@
 # Author: Miguel Alvarez
 ################################################################################
 
-pixel_dim <- function(raster) {
+# Generic function
+setGeneric("pixel_dim",
+		function(raster, ...)
+			standardGeneric("pixel_dim")
+)
+
+# Hidden function
+px_dim <- function(raster, projected=FALSE) {
 	ext <- bbox_sp(raster)
-	ext <- spTransform(ext, CRS("+init=epsg:4326"))
-	centr <- coordinates(ext)
-	centr <- c(mean(centr[,1]), mean(centr[,2]))
-	# UTM conversion
-	zones <- cbind(c(1:60), seq(-177, 177, length.out=60))
-	zone <- zones[order(abs(zones[,2] - centr[1]))[1],1]
-	if(centr[2] >= 0) hem <- "north" else hem <- "south"
-	ext <- spTransform(ext, CRS(paste0("+proj=utm +zone=", zone, " +",
-							hem," +datum=WGS84")))
+	if(!projected) {
+		ext <- spTransform(ext, CRS("+init=epsg:4326"))
+		centr <- coordinates(ext)
+		centr <- c(mean(centr[,1]), mean(centr[,2]))
+		# UTM conversion
+		zones <- cbind(c(1:60), seq(-177, 177, length.out=60))
+		zone <- zones[order(abs(zones[,2] - centr[1]))[1],1]
+		if(centr[2] >= 0) hem <- "north" else hem <- "south"
+		ext <- spTransform(ext, CRS(paste0("+proj=utm +zone=", zone, " +",
+								hem," +datum=WGS84")))
+	}
 	# euclidean distance
 	ext <- coordinates(ext)
 	OUT <- c(mean_height=mean(c(dist(ext[c(2,1),]), dist(ext[c(4,3),]))),
@@ -21,3 +30,16 @@ pixel_dim <- function(raster) {
 	OUT <- OUT/dim(raster)[1:2]
 	return(OUT)
 }
+
+# Define method for RasterLayer etc.
+setMethod("pixel_dim", signature(raster="RasterLayer"),
+		function(raster, projected=FALSE, ...) px_dim(raster, projected)
+)
+
+setMethod("pixel_dim", signature(raster="RasterLayer"),
+		function(raster, projected=FALSE, ...) px_dim(raster, projected)
+)
+
+setMethod("pixel_dim", signature(raster="RasterLayer"),
+		function(raster, projected=FALSE, ...) px_dim(raster, projected)
+)

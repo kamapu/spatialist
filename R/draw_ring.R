155904783a33d89d806eecd40dfa15d5cc2bc3c4
PC 7 <PC 7@131.220.67.49>
2019-06-27 08:53:18 +0200

    README file and bug fixed at draw_ring

--- R/draw_ring.R
+++ R/draw_ring.R
@@ -11,8 +11,8 @@ draw_ring <- function(inner, outer, squared=FALSE) {
 	c_ind <- matrix(rep(1:(outer*2 + 1), outer*2 + 1), nrow=outer*2 + 1,
 			ncol=outer*2 + 1, byrow=TRUE)
 	if(squared) {
-		r_dist <- abs(r_ind - outer - 1) > inner
-		c_dist <- abs(c_ind - outer - 1) > inner
+		r_dist <- abs(r_ind - outer - 1) >= inner
+		c_dist <- abs(c_ind - outer - 1) >= inner
 		win <- r_dist | c_dist
 	} else {
 		Dist <- sqrt(((outer + 1) - r_ind)^2 + ((outer + 1) - c_ind)^2)
---
+++ README.md
@@ -0,0 +1,20 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# spatialist
+
+[![Travis Build
+Status](https://travis-ci.org/kamapu/spatialist.svg?branch=master)](https://travis-ci.org/kamapu/spatialist)
+
+A package for customized spatial (GIS) functions in **R**. To install
+it, use the package `devtools`.
+
+``` r
+library(devtools)
+install_github("kamapu/spatialist")
+```
+
+# Tasks
+
+  - \[ \] To do
+  - \[x\] Done
